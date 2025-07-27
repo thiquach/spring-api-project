d783fee49c71ba5ed656476da50ce77cc144ee58
Thi Quach <amylyquach@gmail.com>
2025-07-27 15:02:13 +1000

    add student controller endpoint /api/v1/student

--- src/main/java/com/springboot/springapiproject/SpringApiProjectApplication.java
+++ src/main/java/com/springboot/springapiproject/SpringApiProjectApplication.java
@@ -1,31 +1,13 @@
 package com.springboot.springapiproject;
 
-import com.springboot.springapiproject.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringApiProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApiProjectApplication.class, args);
     }
 
-    @GetMapping
-    public List<Student> hello(){
-        return List.of(new Student(
-                1L,
-                "john",
-                "johnDoe@gmail.com",
-                LocalDate.of(2000, Month.APRIL, 5),
-                25
-        ));
-    }
 }
