1485f59cfce85030591f68f62ffcb71a1382c53b
Thi Quach <amylyquach@gmail.com>
2025-07-27 10:19:50 +1000

    spring boot api project initial commit

---
+++ src/main/java/com/springboot/springapiproject/SpringApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springapiproject/SpringApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
