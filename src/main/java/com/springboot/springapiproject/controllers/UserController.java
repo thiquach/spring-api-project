9c7179d8de62f351a7b4a0f130a4a434005a30fb
Thi Quach <amylyquach@gmail.com>
2025-07-29 18:12:53 +1000

    add users endpoint

--- src/main/java/com/springboot/springapiproject/controllers/UserController.java
+++ src/main/java/com/springboot/springapiproject/controllers/UserController.java
@@ -2,19 +2,38 @@ package com.springboot.springapiproject.controllers;
 
 import com.springboot.springapiproject.entities.Users;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping(path="/users")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    @GetMapping("/users")
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping
     public List<Users> getUsers() {
-        return userRepository.findAll();
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUsers(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
