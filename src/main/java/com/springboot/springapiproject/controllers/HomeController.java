326402b23941d6cd106956b08bff11b84c8ea9f9
Thi Quach <amylyquach@gmail.com>
2025-07-29 11:29:23 +1000

    add dynamic data using thymeleaf templates

--- src/main/java/com/springboot/springapiproject/controllers/HomeController.java
+++ src/main/java/com/springboot/springapiproject/controllers/HomeController.java
@@ -1,18 +1,15 @@
 package com.springboot.springapiproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
-    }
-
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Amy");
+        return "index";
     }
 }
